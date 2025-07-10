b42eff7f234ce0e6b0c3b3d35b4a99b1068e0d39
William Hayes Barber <barberhayes@gmail.com>
2025-07-10 19:31:47 -0400

    Initial implementation (#1)

---
+++ src/MicroStorage.h
@@ -0,0 +1,66 @@
+#pragma once
+#include <Arduino.h>
+#include <Preferences.h>
+#include <tuple>
+#include <utility>
+
+class MicroStorage {
+public:
+    template<typename... Pairs>
+    static auto get(const char *ns, Pairs&&... pairs) -> std::tuple<decltype(pairs.get(std::declval<Preferences&>()))...> {
+        Preferences prefs;
+        prefs.begin(ns, true);
+        using TupleType = std::tuple<decltype(pairs.get(prefs))...>;
+        TupleType values = std::make_tuple(pairs.get(prefs)...);
+        prefs.end();
+        return values;
+    }
+
+    template<typename... Pairs>
+    static void set(const char *ns, const Pairs&... pairs) {
+        Preferences prefs;
+        prefs.begin(ns, false);
+        (pairs.set(prefs), ...);
+        prefs.end();
+    }
+};
+
+template<typename T>
+class Entry {
+protected:
+    const char *key;
+    T value;
+
+public:
+    Entry(const char *key, const T& value) : key(key), value(value) {}
+    virtual ~Entry() = default;
+
+    virtual T get(Preferences& prefs) const = 0;
+    virtual void set(Preferences& prefs) const = 0;
+};
+
+class StringEntry : public Entry<String> {
+public:
+    StringEntry(const char* key, const String& value) : Entry<String>(key, value) {}
+
+    String get(Preferences& prefs) const override {
+        return prefs.getString(key, value);
+    }
+
+    void set(Preferences& prefs) const override {
+        prefs.putString(key, value);
+    }
+};
+
+class IntEntry : public Entry<int> {
+public:
+    IntEntry(const char* key, int value) : Entry<int>(key, value) {}
+
+    int get(Preferences& prefs) const override {
+        return prefs.getInt(key, value);
+    }
+
+    void set(Preferences& prefs) const override {
+        prefs.putInt(key, value);
+    }
+};
